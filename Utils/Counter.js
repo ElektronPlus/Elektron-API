48f103a7cff2870b590371c771763fd262deadaa
Krystian Wybranowski <krystian.wybranowski@gmail.com>
2021-08-21 13:59:24 +0200

    2022 vacation date

--- Utils/Counter.js
+++ Utils/Counter.js
@@ -4,8 +4,8 @@ moment.locale('pl');
 module.exports = {
     countVacation: function () {
         let now = moment().utc().local()
-        let start = moment("2021-08-14", "YYYY-MM-DD");
-        let end = moment("2021-08-20", "YYYY-MM-DD");
+        let start = moment("2021-09-01", "YYYY-MM-DD");
+        let end = moment("2022-06-24", "YYYY-MM-DD");
         let daysTotal = end.diff(start, 'days')
         let daysLeft = end.diff(now, 'days')+1
         let procent = 1 - (daysLeft / daysTotal)
