702df6f2d20914ca8a7dba841373e5d671a39a1b
Mateusz Tatko <60148744+mtatko@users.noreply.github.com>
2023-01-05 15:03:45 +0100

    fix: :bug: Fix vacation counter
    The vacation counter was displaying strange 'remaining days' due to not handling the change of calendar year during the school year.

--- Utils/Counter.js
+++ Utils/Counter.js
@@ -2,29 +2,49 @@ const moment = require('moment');
 moment.locale('pl');
 
 const lastFridayForMonth = function (monthMoment) {
-    var lastDay = monthMoment.endOf('month').startOf('day');
-    switch (lastDay.day()) {
-      case 6:
-        return lastDay.subtract(1, 'days');
-      default:
-        return lastDay.subtract(lastDay.day() + 2, 'days');
-    }
+  var lastDay = monthMoment.endOf('month').startOf('day');
+  switch (lastDay.day()) {
+    case 6:
+      return lastDay.subtract(1, 'days');
+    default:
+      return lastDay.subtract(lastDay.day() + 2, 'days');
   }
+}
 
 module.exports = {
-    countVacation: function () {
-        let now = moment().utc().local()
-        if (now.month() === 6 || now.month() === 7) {
-            return { daysTotal: 0, daysLeft: 0, procent: 1 };
-        }
-        let start = moment().year(now.year()).month(8).date(1).day(8);
-        if (start.date() > 7) start.day(-6);
-        let end = lastFridayForMonth(moment().year(now.year() + 1).month(5));
-
-        let daysTotal = end.diff(start, 'days')
-        let daysLeft = end.diff(now, 'days')+1
-        let procent = 1 - (daysLeft / daysTotal)
-
-        return ({daysTotal: daysTotal, daysLeft: daysLeft, procent: procent})
+  countVacation: function () {
+    let now = moment().utc().local()
+    if (now.month() === 6 || now.month() === 7) {
+      return { daysTotal: 0, daysLeft: 0, procent: 1 };
     }
+    if (now.month() <= 5) {
+      const start = moment()
+        .year(now.year() - 1)
+        .month(8)
+        .date(1)
+        .day(8);
+      if (start.date() > 7) start.day(-6);
+      const end = lastFridayForMonth(moment().year(now.year()).month(5));
+
+      const daysTotal = end.diff(start, "days");
+      const daysLeft = end.diff(now, "days") + 1;
+      const procent = 1 - daysLeft / daysTotal;
+
+      return { daysTotal: daysTotal, daysLeft: daysLeft, procent: procent };
+    } else {
+      const start = moment().year(now.year()).month(8).date(1).day(8);
+      if (start.date() > 7) start.day(-6);
+      const end = lastFridayForMonth(
+        moment()
+          .year(now.year() + 1)
+          .month(5)
+      );
+
+      const daysTotal = end.diff(start, "days");
+      const daysLeft = end.diff(now, "days") + 1;
+      const procent = 1 - daysLeft / daysTotal;
+
+      return { daysTotal: daysTotal, daysLeft: daysLeft, procent: procent };
+    }
+  }
 }
